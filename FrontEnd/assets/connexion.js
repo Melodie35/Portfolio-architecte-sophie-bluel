6dc997ac29e5ec8d1470011674992ee94fd7b458
Melodie <melodie.migault@gmail.com>
2025-03-26 19:49:03 +0100

    Page de connexion : Récupération du token, message d'erreur si mauvaise connexion et styles

--- FrontEnd/assets/connexion.js
+++ FrontEnd/assets/connexion.js
@@ -1,41 +1,52 @@
 
 
-
+// soumettre le formulaire
 document.querySelector("#connexion").addEventListener("submit", (event) => {
     event.preventDefault()
+    //Récupérer les valeurs du formulaire et les convertir en chaîne de caractère
     const login = {
         email: event.target.querySelector("#email").value,
         password: event.target.querySelector("#password").value
     }
-    const chargeUtile = JSON.stringify(login)
+    const credentials = JSON.stringify(login)
 
+    // Appeler l'API de connexion avec les informations nécessaires
     fetch("http://localhost:5678/api/users/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: chargeUtile
+        body: credentials
     })
+        // Récupérer la réponse de l'API en format JSON
         .then((response) => {
             response.json()
-            if (response.ok) {
-                location = "index.html"
-                //stocker token
-            } else {
-                console.log("erreur de connexion")
-                //message d'erreur, <p id> à mettre en HTML
-            }
-            
+            //Récupérer les données de l'API
+            .then ((data) => {
+                // si la réponse est OK
+                if (response.ok) {
+                    // stocker le token
+                    localStorage.getItem("token", data.token)
+                    // revenir à la page d'accueil
+                    location = "index.html"
+                // sinon, afficher le message d'erreur
+                } else {
+                    document.querySelector("#error-message").style.display = "block"                    
+                }
+            })
         })
-        
-
+    
         .catch((err) => {
             console.log("dans le catch")
             console.log(err)
         })
-        
 
-        
-    console.log(chargeUtile)
+    console.log(credentials)
     console.log("formulaire soumis")
-
-
 })
+
+        
+
+        
+        
+
+        
+ 
