591778d36d3a1d71556dbfae97ddee20a29ac4d5
Melodie <melodie.migault@gmail.com>
2025-04-04 13:56:15 +0200

    Contenu des modales et CSS

--- FrontEnd/assets/modal.js
+++ FrontEnd/assets/modal.js
@@ -1,15 +1,19 @@
-const openModal = (e) =>  {
+//Fonction pour ouvrir la 1ère modale
+document.querySelector("#modifier").addEventListener("click", (e) => {
     e.preventDefault()
     const target = document.querySelector(e.target.getAttribute("href"))
     target.style.display = null
     target.removeAttribute("aria-hidden")
     target.setAttribute("aria-modal", "true")
+    //Gérer les options de fermeture de la modale
     modal = target
     modal.addEventListener("click", closeModal)
     modal.querySelector(".js-xmark").addEventListener("click", closeModal)
     modal.querySelector(".js-modal-stop").addEventListener("click", stopPropagation)
-}
+})
+    
 
+//Fonction pour fermer la modale
 const closeModal = (e) => {
     if (modal === null) return
     e.preventDefault()
@@ -22,29 +26,70 @@ const closeModal = (e) => {
     modal = null
 }
 
+//Fonction pour éviter de fermer la modale lorsqu'on clique dedans
 const stopPropagation = (e) => {
     e.stopPropagation()
 }
 
-document.querySelectorAll(".js-modal").forEach(a =>{
-    a.addEventListener("click", openModal)
-})
+ //Charger la gallerie dans 1ère modale
+ fetch("http://localhost:5678/api/works")
+ .then((response) => response.json())
+ .then((data) => {
+     let gallery = ""
+     for (let figure of data) {
+         gallery += `
+             <figure class="fig-modal" data-fig="${figure.category.id}">
+                 <i class="fa-solid fa-trash-can"></i>
+                 <img src="${figure.imageUrl}" alt="${figure.title}">                        
+             </figure>
+         `
+     }
+     document.querySelector("#gallery-modal").insertAdjacentHTML("beforeend", gallery)
+ })
+ .catch((err) => {
+     console.log(err)
+ })
 
+//Passer à la 2nde modale après avoir cliqué sur Ajouter une photo
 document.querySelector("#add-a-photo").addEventListener("click", (e) => {
     e.preventDefault()    
     document.querySelector("#gallery-photo").style.display = "none"
     document.querySelector("#add-photo").style.display = null
-    document.querySelector("#js-arrow-left").style.display = null    
+    document.querySelector("#js-arrow-left").style.color = "black"
+    //Gérer la fermeture de la 2nde modale
+    document.querySelector("#modal-gallery").addEventListener("click", (e) => {
+        document.querySelector("#gallery-photo").style.display = null
+        document.querySelector("#add-photo").style.display = "none"
+        document.querySelector("#js-arrow-left").style.color = "white"
+    })
     document.querySelector(".js-xmark").addEventListener("click", (e) => {
         document.querySelector("#gallery-photo").style.display = null
         document.querySelector("#add-photo").style.display = "none"
-        document.querySelector("#js-arrow-left").style.display = "none"
-    })   
+        document.querySelector("#js-arrow-left").style.color = "white"
+    })    
 })
 
+//Passer à la 1ère modale lorsqu'on clique sur la flèche gauche
 document.querySelector("#js-arrow-left").addEventListener("click", (e) => {
     e.preventDefault()
     document.querySelector("#gallery-photo").style.display = null
     document.querySelector("#add-photo").style.display = "none"
-    document.querySelector("#js-arrow-left").style.display = "none"
+    document.querySelector("#js-arrow-left").style.color = "white"
+})
+
+
+//Charger les catégories dans la 2nde modale
+fetch("http://localhost:5678/api/categories")
+.then((response) => response.json())
+.then((data) => {
+    const blankOption = `
+        <option value="blank"></option>
+    `
+    let optionCategory = ""
+    for (let option of data) {
+        optionCategory += `
+            <option value="${option.name}">${option.name}</option>
+        `
+    }
+    document.querySelector("#category").insertAdjacentHTML("beforeend", blankOption+optionCategory)
 })
